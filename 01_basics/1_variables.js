34a7197ca5a39c78666c958c06c401a75abf360f
sumantpolai <137427323+sumantpolai@users.noreply.github.com>
2024-05-09 05:22:52 +0000

    variables

---
+++ 01_basics/1_variables.js
@@ -0,0 +1,12 @@
+const accountId=12345
+let accountEmail="sumantpolai@gmail.com"
+let accountPassword="123"
+let accountCity="Bhubaneswar"
+let accountState;
+
+
+//console.log(accountId);
+accountEmail="polai@gmail.com"
+accountPassword="234"
+accountCity="berhampur"
+console.table([accountEmail,accountPassword,accountCity,accountState]);
