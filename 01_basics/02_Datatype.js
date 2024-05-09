87453d559f564a2bb002481f30299249599f77e3
sumantpolai <137427323+sumantpolai@users.noreply.github.com>
2024-05-09 05:23:42 +0000

    datatype

---
+++ 01_basics/02_Datatype.js
@@ -0,0 +1,17 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs ,not browser
+
+//number => 2 to the power 35
+// bigint
+// string => ""
+// boolean => true/false
+// Null => standalone value
+// undifine => 
+// sybmbol => unique
+
+              //Object//
+
+console.log(typeof"sumant");  //string
+console.log(typeof null);     //object
+console.log(typeof undefined); //undefined        
