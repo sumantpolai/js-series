b3672dac1a2dde9ecad06b2279aa4401940970aa
sumantpolai <137427323+sumantpolai@users.noreply.github.com>
2024-05-12 16:22:44 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,13 @@
+const name="sumanta"
+const repoCount=50
+
+// console.log(name+repoCount+"value");
+
+console.log(`my name is ${name} and my repo count is${repoCount}`);     // morden way or string interpolation
+
+
+//another way to represent string
+
+const gameName= new String('sumanta')    //benifit is that we can get some extra information like we print each char. or we know about some fun.
+
+console.log(gameName[0]);  // h
