24a0cd86a7fec33357cd204da894b04f2952cd34
sumantpolai <137427323+sumantpolai@users.noreply.github.com>
2024-05-12 15:04:21 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+    //  primitive
+
+// 7 types : String , Number , Boolean , undefined , null , Symbol , BigInt
+
+const score =234;
+const scoreValue =234.0;
+const isLoggedIn =false;
+let userEmail;
+const outsideTemp=null;
+const id =Symbol('123');
+const anotherId =Symbol('123')    // here id /= anotherId
+const bigNumber=12345443223345252525262n;
+
+
+
+    // Reference (Non primitive) 
+    
+// Array , Objects , Functions 
+
+const heros =["saktiman","nagraj","doga"];
+
+let myObj ={
+    name :"sumant",
+    age: 21,
+}
+const myFunction=function(){
+    console.log("Hello World");
+}
+        
