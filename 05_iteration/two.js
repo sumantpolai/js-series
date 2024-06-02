39f13f63c26b65fc264490ecf95386b045f2d34c
sumantpolai <137427323+sumantpolai@users.noreply.github.com>
2024-06-02 08:41:12 +0000

    do while

---
+++ 05_iteration/two.js
@@ -0,0 +1,19 @@
+//=======  while and do while ========
+
+let index = 0
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index = index+2
+    
+}
+
+
+//=== do while
+
+let score = 1
+do{
+    console.log(`score is ${score}`);
+    score++
+}while (score <= 10) {
+    
+}
