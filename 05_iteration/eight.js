c44f09c1b57bd1c252ab62ae490406d86d3b71b0
sumantpolai <137427323+sumantpolai@users.noreply.github.com>
2024-06-02 10:55:53 +0000

    reduce

---
+++ 05_iteration/eight.js
@@ -0,0 +1,31 @@
+const myNums =[1,2,3]
+
+// const myTotal = myNums.reduce(function (acc, currval){
+//     console.log(`acc: ${acc}, and curval: ${currval}`);
+//     return acc + currval
+// },0)   // after comma whatever value we gave that is acc
+
+// console.log(myTotal);
+
+
+const myTotal = myNums.reduce((acc, curval) => acc+curval,0)   //another way
+console.log(myTotal);
+
+
+const shoppingCart = [
+ {
+    itemName: "js course",
+    price: 2999
+ },
+ {
+    itemName: "mobile dev course",
+    price: 4999
+ },
+ {
+    itemName: "data science course",
+    price: 12999
+ }
+]
+
+const priceToPay = shoppingCart.reduce( (item) => acc + item.price,0)
+console.log(priceToPay);  //here add all price and return that
