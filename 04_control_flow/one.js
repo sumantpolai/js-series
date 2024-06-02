a1aa3c2de5ed010803b95ba0610d8159f82e5c79
sumantpolai <137427323+sumantpolai@users.noreply.github.com>
2024-06-02 04:13:05 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,52 @@
+// if
+
+
+const temperature = 43
+
+if (temperature< 50){
+    console.log("less then 50");
+
+} else
+console.log("greater then 50");
+
+// === is called strictly equaled it check data type also
+
+
+
+//==== switch ======
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }     // syntax
+
+
+const month =3
+switch (month) {
+    case 1:
+        console.log(january);
+        
+        break;
+    case 2:
+        console.log(feb);
+        
+        break;
+    case 3:
+        console.log(march);
+        
+        break;
+    case 4:
+        console.log(april);
+        
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
