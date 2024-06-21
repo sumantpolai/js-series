8c6cd10606c9c6671925506743a11b108e3c6fdf
sumantpolai <137427323+sumantpolai@users.noreply.github.com>
2024-06-21 04:29:13 +0000

    mathpi

---
+++ 10_classes_oops/09_mathpi.js
@@ -0,0 +1,34 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+// console.log(descripter);
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("chai nhi bni");
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));  // here we describe rhe property of name which is present in chai
+
+Object.defineProperty(chai, 'name', {    // here we can change the property
+    //writable: false,
+    enumerable: true,  // if here we write false here then we can not enumerate and iterate this property
+    
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        
+        console.log(`${key} : ${value}`);
+    }
+}
